6aa15c782b681a08d47894fb0a62fc2ba931c334
Igor Datsenko <datsenko@MacBook-Air-Igor.local>
2022-09-04 23:08:11 +0300

    add useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './index.scss'
 import Users from './components/users';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [users, SetUsers] = useState();
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      SetUsers(json.data);
+    }).catch(err => {
+      console.warn(err);
+      alert('Помилка при отриманні користувачів')
+    })
+  }, [])
+
   return (
     <div className="App">
       <Users/>
